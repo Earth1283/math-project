90b85dc023c32b3a6a359fbc121a893c7f59ada1
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2026-05-16 05:27:17 +0800

    feat: animate best fit math and residuals

--- linear-explainer.py
+++ linear-explainer.py
@@ -35,3 +35,31 @@ class LinearRegressionExplainer(Scene):
         dots = VGroup(*[Dot(axes.c2p(x_val, y_val), color=BLUE) for x_val, y_val in zip(X, y)])
         self.play(FadeIn(dots, lag_ratio=0.1))
         self.wait(1)
+        
+        # 3. Best Fit Math and Residuals
+        m_guess, b_guess = 0.005, -1.5
+        equation = Text("y = mx + b").to_corner(UR).shift(LEFT * 0.5)
+        
+        guess_line = axes.plot(
+            lambda x: m_guess * x + b_guess,
+            color=YELLOW,
+            x_range=[310, 420]
+        )
+        
+        self.play(Write(equation))
+        self.play(Create(guess_line))
+        self.wait(1)
+        
+        # Visualize Residuals
+        residuals = VGroup(*[
+            DashedLine(
+                start=dots[i].get_center(),
+                end=axes.c2p(X[i], m_guess * X[i] + b_guess),
+                color=RED,
+                stroke_width=2
+            )
+            for i in range(len(X))
+        ])
+        
+        self.play(Create(residuals, lag_ratio=0.05))
+        self.wait(2)
